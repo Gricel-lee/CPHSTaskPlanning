a8a0db5f11d3eb3c787fd321dc9fe22cfd2dc8aa
Gricel-lee <gnvf500@york.ac.uk>
2025-12-17 20:26:24 +0000

    full jobs list

--- launch_all_jobs_ExpAgricultural_PropMin.sh
+++ launch_all_jobs_ExpAgricultural_PropMin.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 echo 'Submitting jobscript_ExpAgricultural_PropMin1.job'
 sbatch jobscript_ExpAgricultural_PropMin1.job
 echo 'Submitting jobscript_ExpAgricultural_PropMin2.job'
@@ -20,3 +19,23 @@ echo 'Submitting jobscript_ExpAgricultural_PropMin9.job'
 sbatch jobscript_ExpAgricultural_PropMin9.job
 echo 'Submitting jobscript_ExpAgricultural_PropMin10.job'
 sbatch jobscript_ExpAgricultural_PropMin10.job
+echo 'Submitting jobscript_ExpAgricultural_PropMin11.job'
+sbatch jobscript_ExpAgricultural_PropMin11.job
+echo 'Submitting jobscript_ExpAgricultural_PropMin12.job'
+sbatch jobscript_ExpAgricultural_PropMin12.job
+echo 'Submitting jobscript_ExpAgricultural_PropMin13.job'
+sbatch jobscript_ExpAgricultural_PropMin13.job
+echo 'Submitting jobscript_ExpAgricultural_PropMin14.job'
+sbatch jobscript_ExpAgricultural_PropMin14.job
+echo 'Submitting jobscript_ExpAgricultural_PropMin15.job'
+sbatch jobscript_ExpAgricultural_PropMin15.job
+echo 'Submitting jobscript_ExpAgricultural_PropMin16.job'
+sbatch jobscript_ExpAgricultural_PropMin16.job
+echo 'Submitting jobscript_ExpAgricultural_PropMin17.job'
+sbatch jobscript_ExpAgricultural_PropMin17.job
+echo 'Submitting jobscript_ExpAgricultural_PropMin18.job'
+sbatch jobscript_ExpAgricultural_PropMin18.job
+echo 'Submitting jobscript_ExpAgricultural_PropMin19.job'
+sbatch jobscript_ExpAgricultural_PropMin19.job
+echo 'Submitting jobscript_ExpAgricultural_PropMin20.job'
+sbatch jobscript_ExpAgricultural_PropMin20.job
